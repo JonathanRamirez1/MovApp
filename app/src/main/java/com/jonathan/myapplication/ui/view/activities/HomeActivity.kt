e430a9aa73908a9f259cb0ba98105b1acefb5ae2
Jonathan Javier Ramirez <jonathanramirez@wposs.com>
2022-03-18 16:57:41 -0500

    Se creo interfaz de splash screen

--- app/src/main/java/com/jonathan/myapplication/ui/view/activities/HomeActivity.kt
+++ app/src/main/java/com/jonathan/myapplication/ui/view/activities/HomeActivity.kt
@@ -4,15 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.jonathan.myapplication.databinding.ActivityMainBinding
+import com.jonathan.myapplication.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/jonathan/myapplication/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.jonathan.myapplication.ui.viewmodel
+
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+class HomeViewModel {
+
+}
