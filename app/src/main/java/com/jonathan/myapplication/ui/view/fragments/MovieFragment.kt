d64614313cc4e686ce37ef24e1dd191f44d83de3
Jonathan Ramirez <52109489+JonathanRamirez1@users.noreply.github.com>
2022-03-20 14:45:46 -0500

    Se creo la interfaz Movie

--- app/src/main/java/com/jonathan/myapplication/ui/view/fragments/MovieFragment.kt
+++ app/src/main/java/com/jonathan/myapplication/ui/view/fragments/MovieFragment.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.jonathan.myapplication.databinding.FragmentMovieBinding
 import com.jonathan.myapplication.ui.viewmodel.MovieViewModel
+import com.jonathan.myapplication.util.Constants.IMAGE_URL
 import com.jonathan.myapplication.util.Constants.MOVIE_ID
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,26 +18,33 @@ import dagger.hilt.android.AndroidEntryPoint
 class MovieFragment : Fragment() {
 
     private lateinit var binding: FragmentMovieBinding
-
     private val movieViewModel: MovieViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentMovieBinding.inflate(layoutInflater, container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setObserver()
+    }
 
+    private fun setObserver() {
+        val backdrop = binding.imageViewBackdropPath
         val movieId = arguments?.getLong(MOVIE_ID) ?: 0
+
         if (movieId == 0L)
             errorOperation()
 
         movieViewModel.getDetail(movieId).observe(viewLifecycleOwner, { movie ->
-            /*Glide.with(this).load(IMAGE_URL + movie.backdrop_path)
-                .into()*/
 
+            Glide.with(this).load(IMAGE_URL + movie.backdrop_path).into(backdrop)
             binding.materialTextViewMovieTitle.text = movie.title
+            binding.materialTextViewOverview.text = movie.overview
+            binding.materialTextViewVoteCount.text = movie.vote_count
+            binding.materialTextViewDate.text = movie.release_date
+            binding.materialTextViewPopularity.text = movie.popularity
         })
     }
 
